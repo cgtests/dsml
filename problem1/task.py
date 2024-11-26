b32bd4590c488b27008d0549a956442faf69e168
cgtests <ausgefuhrt12@gmail.com>
2024-11-26 23:34:53 +0530

    Create task.py

---
+++ problem1/task.py
@@ -0,0 +1,7 @@
+"""
+Perform the following operations using Python on a data set : read data
+from different formats(like csv, xls),indexing and selecting data, sort data,
+describe attributes of data, checking data types of each column. (Use
+Titanic Dataset).
+"""
+
